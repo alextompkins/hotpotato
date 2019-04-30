09b7a56183233dfc5ba28421c402766dd1cda2dc
Tyler Kennedy <tke34@uclive.ac.nz>
2019-05-01 10:49:36 +1200

    Add paged main activity with menu fragments
    Added a host game and join game fragment with simple placeholder text. These are contained in a view pager which allows swapping between these.
    Fragments are created programmatically not declaratively, which may be an issue later.
    
    Added a TODO around hardcoded text strings, will need to change this to a strings.xml reference later although that may be require a workaround (read: hack)

--- app/src/main/java/me/nubuscu/hotpotato/MainActivity.kt
+++ app/src/main/java/me/nubuscu/hotpotato/MainActivity.kt
@@ -2,11 +2,24 @@ package me.nubuscu.hotpotato
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: MenuPagerAdapter
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager: ViewPager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewPager = findViewById(R.id.viewPager)
+        tabLayout = findViewById(R.id.tabLayout)
+
+        adapter = MenuPagerAdapter(supportFragmentManager)
+        viewPager.adapter = adapter
+        tabLayout.setupWithViewPager(viewPager)
+
     }
 }
---
+++ app/src/main/java/me/nubuscu/hotpotato/MenuPagerAdapter.kt
@@ -0,0 +1,24 @@
+package me.nubuscu.hotpotato
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import me.nubuscu.hotpotato.menu.HostGameFragment
+import me.nubuscu.hotpotato.menu.JoinGameFragment
+
+class MenuPagerAdapter(fm: FragmentManager): FragmentPagerAdapter(fm) {
+
+    private var fragments = listOf(JoinGameFragment(), HostGameFragment())
+    private var fragmentTitles = listOf("Join Game", "Host Game") //TODO set titles via static string values
+    override fun getItem(position: Int): Fragment {
+        return fragments[position]
+    }
+
+    override fun getCount(): Int {
+        return fragments.size
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return fragmentTitles[position]
+    }
+}
