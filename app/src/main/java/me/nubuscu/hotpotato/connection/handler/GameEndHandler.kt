be8ed5d51984504fd77bd5eb5c1428e1eca3cef6
Alex Tompkins <ato47@uclive.ac.nz>
2019-06-07 10:38:42 +1200

    Hotfix for GameEndMessages not propagating through host correctly

--- app/src/main/java/me/nubuscu/hotpotato/connection/handler/GameEndHandler.kt
+++ app/src/main/java/me/nubuscu/hotpotato/connection/handler/GameEndHandler.kt
@@ -1,5 +1,22 @@
 package me.nubuscu.hotpotato.connection.handler
 
 import me.nubuscu.hotpotato.model.dto.GameEndMessage
+import me.nubuscu.hotpotato.util.DataHolder
+import me.nubuscu.hotpotato.util.GameInfoHolder
+import me.nubuscu.hotpotato.util.sendToNearbyEndpoints
 
-object GameEndHandler : PayloadHandler<GameEndMessage>()
+object GameEndHandler : PayloadHandler<GameEndMessage>() {
+    override fun handle(message: GameEndMessage) {
+        super.handle(message)
+
+        if (GameInfoHolder.instance.isHost) {
+            val info = GameInfoHolder.instance
+
+            sendToNearbyEndpoints(message,
+                info.endpoints
+                    .map { it.id }
+                    .filter { it != info.myEndpointId && it != message.loserEndpointId },
+                DataHolder.instance.context.get()!!)
+        }
+    }
+}
